00cbe4e2669326cd259d9cd369e5e38b8f295dba
Improver2108 <yashrawat2108@gmail.com>
2024-03-20 22:10:59 +0530

    change npm to pnpm

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,15 +9,20 @@ type HeaderProp = {
 const Header = ({ children }: HeaderProp) => {
   const [navVisible, isNavVisible] = useState(false);
   return (
-    <>
+    <header>
       <section className="z-[70] flex h-[3.25em] items-center justify-between bg-green-400 px-3">
         <h1>Logo</h1>
         <button className="text-xl" onClick={() => isNavVisible(!navVisible)}>
           <RxHamburgerMenu />
         </button>
       </section>
-      {navVisible && <section className="animate-slide-in">{children}</section>}
-    </>
+      <section
+        className={`overflow-hidden bg-green-400 transition-[height] duration-[500] ease-in-out ${navVisible ? "h-[11em]" : "h-0"} `}
+      >
+        {children}
+      </section>
+      hi
+    </header>
   );
 };
 export default Header;
