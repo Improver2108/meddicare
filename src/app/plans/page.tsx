8247673cdc33533e4538af11cf079ca11f7abb22
Improver2108 <yashrawat2108@gmail.com>
2024-04-23 20:22:58 +0530

    "Replace standard/premium plan divs with a flip card component"

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -3,20 +3,14 @@ const Plans = () => {
     <main className="flex h-[92vh] flex-col items-center justify-center gap-10 px-5">
       <h1 className="text-2xl font-semibold">Plans</h1>
       <section className="flex w-full flex-col gap-5 text-center">
-        <div className="flex h-[15rem] cursor-pointer flex-col items-center justify-center gap-4 rounded-lg bg-gradient-to-r from-[#9dcfd3] to-[#629ea7] p-10">
-          <h1 className="text-xl font-semibold">Standard Plan</h1>
-          <p className="text-lg">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem,
-            labore.
-          </p>
-        </div>
-        <div className="flex h-[15rem] cursor-pointer flex-col items-center justify-center gap-4 rounded-lg bg-gradient-to-r from-[#9dcfd3] to-[#629ea7] p-10">
-          <h1 className="text-xl font-semibold">Premium Plans</h1>
-          <p className="text-lg">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem,
-            labore.
-          </p>
-        </div>
+        <article className="flex h-[15rem] cursor-pointer flex-col  items-center justify-center shadow-lg transition-transform duration-500 [transform-style:preserve-3d] hover:transition-transform hover:[transform:rotateY(180deg)]">
+          <div className="absolute bg-white [backface-visibility:hidden]">
+            Front
+          </div>
+          <div className="absolute bg-[#03446A] text-white [transform:rotateY(180deg)]">
+            Back
+          </div>
+        </article>
       </section>
     </main>
   );
