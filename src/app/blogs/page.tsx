a6678654c1c5f20700d8f104e8f1fe80bc8efc9d
dhaval78 <dhavi78@gmail.com>
2024-04-23 11:44:32 +0530

    fixed blog

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,21 +1,62 @@
+"use client"
 import Link from "next/link";
+import "../blogs/gg.css";
+import { useEffect, useState, useRef } from "react";
+import { types } from "util";
+
 
 const Plans = () => {
+  const arr = [...Array<null>(5)];
+  const delay = 2500;
+ 
+
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef<number | null>(null)
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = window.setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === arr.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+
+
   return (
     <main className="flex flex-col items-center gap-10 px-8 py-3">
       <h1 className="mt-3 text-2xl font-bold">Blogs</h1>
       <section className="w-full space-y-2">
-        {[...Array<null>(15)].map((e, i) => (
-          <article
-            key={i}
-            className="border-1 rounded-lg bg-gray-300 px-3 py-2 hover:bg-gray-500"
+        <div className="slideshow">
+          <div
+            className="slideshowSlider"
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
           >
-            <Link href={`/blogs/${i}`} key={i}>
-              <div className="space-y-1">
+            {arr.map((e, index) => (
+          <div
+            key={index}
+            className="slide"
+            style={{backgroundColor:"lightblue"}}
+          >
+            <Link href={`/blogs/${index}`} key={index}>
+              <br/>
                 <h1 className="text-xl font-semibold">
                   Lorem ipsum dolor sit amet.
                 </h1>
-                <p className="line-clamp-4 text-sm">
+                <p className="line-clamp-4 text-sm " style={{whiteSpace:"pre-line"}} >
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Laborum, eaque. Dolorem fugit eveniet aspernatur unde nesciunt
                   commodi cum quaerat perferendis. Consequatur, quibusdam
@@ -24,12 +65,26 @@ const Plans = () => {
                   accusamus. Dignissimos quae sequi cumque ratione voluptates,
                   quas laboriosam expedita repellendus.
                 </p>
-              </div>
             </Link>
-          </article>
+          </div>
         ))}
+          </div>
+
+          <div className="slideshowDots">
+            {arr.map((_, idx) => (
+              <div
+                key={idx}
+                className={`slideshowDot${index === idx ? " active" : ""}`}
+                onClick={() => {
+                  setIndex(idx);
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
       </section>
     </main>
+
   );
 };
 export default Plans;
